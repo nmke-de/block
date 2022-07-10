30a1789b15ee845c81619d6a697f76175d83f777
nmke-de <github@nmke.de>
2022-07-11 00:23:25 +0200

    Respecting PASS_MAX now

--- block.c
+++ block.c
@@ -1,11 +1,16 @@
+#include <shadow.h>
 #include <pwd.h>
 #include <unistd.h>
 #include "BarnabasBox/Barnabas.h"
 
+#ifndef PASS_MAX
+#define PASS_MAX 512
+#endif
+
 int main() {
 	struct passwd *pw = getpwuid(geteuid());
 	if(!pw) return 1;
-	char buf[512];
+	char buf[PASS_MAX];
 	binit();
 	bquit();
 }
